cc342473d82376faeeb4c58a3d3aac96379a4bd9
Cristina <crispitipina@gmail.com>
2022-09-05 11:35:19 +0200

    files created

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let button = document.querySelector("button");
+let greeting = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+    greeting.innerText = "Hello World! 🙋🏻‍♀️"
+    
+})
